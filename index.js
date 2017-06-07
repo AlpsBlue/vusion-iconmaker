1aa255f02deeb1e9dd77dff3dafeba5d31b6fcc7
hzlinguanfu <770511401@qq.com>
2017-06-07 14:44:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var _ = require('lodash');
 var path = require('path');
 
 module.exports = function (source) {
-    // console.log(source);
     let resourcePath = this.resourcePath;
     let tag = path.basename(resourcePath, '.svg');
     return 'module.exports = ' + tag;
