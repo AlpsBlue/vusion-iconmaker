2f4f0f17f58e00559a6201b0477efdf553b77b0e
林冠夫 <hzlinguanfu@corp.netease.comm>
2017-06-07 11:34:28 +0800

    [add]配套文件

---
+++ index.js
@@ -0,0 +1,8 @@
+var _ = require('lodash');
+var path = require('path');
+
+module.exports = function (source) {
+    // console.log(source);
+    let resourcePath = this.resourcePath;
+    let tag = path.basename(resourcePath, '.svg');
+    return 'module.exports = ' + tag;
